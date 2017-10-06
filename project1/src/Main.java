4ff4a3f38bd4f038d8cc1cc1b33423156d7751dc
Eric Wolfe <rojowolf21@gmail.com>
2017-10-06 18:23:23 -0500

    Modifying ignore file and working on main

---
+++ project1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    private static double[][] transitionMatrix = new double[][]{
+            {0.8, 0.2, 0.0, 0.0},
+            {0.1, 0.8, 0.1, 0.0},
+            {0.1, 0.0, 0.7, 0.2},
+            {0.1, 0.0, 0.0, 0.9}
+    };
+
+    public static void main(String[] args) {
+        
+    }
+
+}

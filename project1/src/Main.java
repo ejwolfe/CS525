f47c449a43b856577b1abd9b322b7c5c688ddb59
Eric Wolfe <rojowolf21@gmail.com>
2017-10-07 11:43:50 -0500

    Finished markov chain class and main

--- project1/src/Main.java
+++ project1/src/Main.java
@@ -1,7 +1,42 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Starting tests: ");
+        // Experiment 1
+        new MarkovChain(0, 30, "test1.csv");
+        System.out.print(":-");
+        new MarkovChain(1, 30, "test2.csv");
+        System.out.print("-");
+        new MarkovChain(2, 30, "test3.csv");
+        System.out.print("-");
+        new MarkovChain(3, 30, "test4.csv");
+        System.out.print("-");
+        // Experiment 2
+        new MarkovChain(100, "test5.csv");
+        System.out.print("-");
+        new MarkovChain(1000, "test6.csv");
+        // Experiment 3
+        System.out.print("-");
+        new MarkovChain(100, "test7.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test8.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test9.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test10.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test11.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test12.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test13.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test14.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test15.csv");
+        System.out.print("-");
+        new MarkovChain(100, "test16.csv");
+        System.out.print("-:\n Finished tests");
     }
 
 }
--- project1/src/MarkovChain.java
+++ project1/src/MarkovChain.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
 import java.util.Random;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 
 public class MarkovChain {
 
@@ -8,29 +11,66 @@ public class MarkovChain {
             {0.1, 0.0, 0.7, 0.2},
             {0.1, 0.0, 0.0, 0.9}
     };
-    private double totalWalks = 0.0;
+    private double totalWalks = 1.0;
     private double gCount = 0.0;
     private double yCount = 0.0;
     private double oCount = 0.0;
     private double rCount = 0.0;
+    private String fileName;
 
-    public MarkovChain(){
-        runRandomStart();
-    }
-
-    public MarkovChain(int startPosition){
-        runKnownStart(startPosition);
-    }
-
-    private void runKnownStart(int startPosition){
-
-    }
-
-    private void runRandomStart(){
+    /*
+    * Constructor that will run a walk with a random initial state
+    */
+    public MarkovChain(int numberOfSteps, String fileName){
+        this.fileName = fileName;
         Random random = new Random();
         int initial = random.nextInt(3);
+        run(initial, numberOfSteps);
     }
 
+    /*
+    * Constructor that will run a walk with a known initial state
+    */
+    public MarkovChain(int startPosition, int numberOfSteps, String fileName){
+        this.fileName = fileName;
+        run(startPosition, numberOfSteps);
+    }
+
+    /*
+    * Function for going through the steps and recording the ratio of times reached a state
+     */
+    private void run(int startPosition, int numberOfSteps){
+        double[] gArray = new double[numberOfSteps];
+        double[] yArray = new double[numberOfSteps];
+        double[] oArray = new double[numberOfSteps];
+        double[] rArray = new double[numberOfSteps];
+        for(int i = 0; i < numberOfSteps; i++, totalWalks++){
+            startPosition = walk(startPosition);
+            switch (startPosition){
+                case 0:
+                    gCount++;
+                    break;
+                case 1:
+                    yCount++;
+                    break;
+                case 2:
+                    oCount++;
+                    break;
+                case 3:
+                    rCount++;
+                    break;
+            }
+            gArray[i] = gCount / totalWalks;
+            yArray[i] = yCount / totalWalks;
+            oArray[i] = oCount / totalWalks;
+            rArray[i] = rCount / totalWalks;
+        }
+        printToCSV(gArray, yArray, oArray, rArray);
+    }
+
+    /*
+    * Function for deciding where the next walk will be
+     */
     private int walk(int currentState){
         // Variable for return
         int nextState = 0;
@@ -49,10 +89,46 @@ public class MarkovChain {
             nextState = 1;
         } else if (nextProbability > case1 && nextProbability <= case2) {
             nextState = 2;
-        } else {
+        } else if (nextProbability > case2 && nextProbability <= case3) {
             nextState = 3;
+        } else {
+            System.out.println("There was an error");
+            System.exit(1);
         }
         // Return the next walk location
         return nextState;
+    }
+
+    /*
+    * Function for printing the results of the walk to a csv file
+     */
+    private void printToCSV(double[] array0, double[] array1, double[] array2, double[] array3){
+        try {
+            FileWriter fileWriter = new FileWriter(fileName);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < array0.length; j++) {
+                    switch (i){
+                        case 0:
+                            printWriter.printf("%.2f,", array0[j]);
+                            break;
+                        case 1:
+                            printWriter.printf("%.2f,", array1[j]);
+                            break;
+                        case 2:
+                            printWriter.printf("%.2f,", array2[j]);
+                            break;
+                        case 3:
+                            printWriter.printf("%.2f,", array3[j]);
+                            break;
+                    }
+                }
+                printWriter.println();
+            }
+            printWriter.close();
+            fileWriter.close();
+        } catch (IOException e){
+            System.out.println(e);
+        }
     }
 }
